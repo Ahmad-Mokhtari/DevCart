723d239b970a16dbac00909a634f4958d7be0144
Mokhtari <mokhtari.master@gmail.com>
2024-08-26 19:58:21 +0330

    be complete insert template

--- DevCart/Controllers/HomeController.cs
+++ DevCart/Controllers/HomeController.cs
@@ -23,7 +23,14 @@ namespace DevCart.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+	 
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
